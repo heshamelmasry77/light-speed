0c57622cdb0f03cc062940877b47dd77efeddc2f
heshamelmasry77 <heshamelmasry1992@gmail.com>
2025-05-04 03:33:29 +0200

    edits

--- src/components/MarsOreMap.tsx
+++ src/components/MarsOreMap.tsx
@@ -52,7 +52,7 @@ const MarsOreMap = ({ acquisitions }: Props) => {
   const allSites = acquisitions.map((acq, i) => generateSiteFromAcquisition(acq, i));
   return (
     <div
-      className="w-96 h-96 rounded-full relative overflow-hidden md:shadow-xl planet-spin bg-contain md:bg-cover bg-no-repeat"
+      className="w-64	h-64 sm:w-72 sm:h-72 md:w-96 md:h-96 rounded-full relative overflow-hidden md:shadow-xl planet-spin bg-contain md:bg-cover bg-no-repeat"
       style={{
         backgroundImage: "url(/mars.png)",
         backgroundPosition: "center",
